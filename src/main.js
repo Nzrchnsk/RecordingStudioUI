6ebc4e6140e18eccdd3ddacbc7e191cee50bb22f
Nikita Petin <nikita.pietin@gmail.com>
2021-06-16 00:02:48 +0300

    RecordingStudio

--- src/main.js
+++ src/main.js
@@ -12,5 +12,11 @@ import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+axios.interceptors.request.use(function (config) {
+    config.headers.common = {
+        'Authorization': `Bearer ${localStorage.getItem('auth')}`,
+    };
+    return config;
+});
 
 createApp(App).use(store).use(VueAxios, axios).use(router).mount('#app')
